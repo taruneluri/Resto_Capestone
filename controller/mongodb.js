0d01e09a390563fa322e5f1a1013472b0de7008b
taruneluri <taruneluri2106@gmail.com>
2024-01-25 13:16:50 +0530

    updated mongoDB

--- controller/mongodb.js
+++ controller/mongodb.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
 const uri = "mongodb+srv://taruneluri:taruneluri@cluster0.gkezw.mongodb.net/resto?retryWrites=true&w=majority";
-mongoose.connect(uri).then(()=>{console.log("DataBase Connected !!")});
+
+const url= "mongodb+srv://srinija:Srinija123@cluster0.1gthf.mongodb.net/resto?retryWrites=true&w=majority";
+mongoose.connect(url).then(()=>{console.log("DataBase Connected !!")});
 module.exports=mongoose;
